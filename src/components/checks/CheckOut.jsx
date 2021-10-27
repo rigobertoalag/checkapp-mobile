c1dc5a27b896855d0910261e93f0f3b6c69b6f35
rigobertoalag <a20150338@utem.edu.mx>
2021-10-26 19:00:05 -0500

    Finish with take and show picture functions

--- src/components/checks/CheckOut.jsx
+++ src/components/checks/CheckOut.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View, Button, Image } from "react-native";
 import { Camera } from "expo-camera";
 
 export default function CheckOut() {
   const [hasPermission, setHasPermission] = useState(null);
-  const camera = Camera
-//   const [type, setType] = useState(Camera.Constants.Type.front); Se deshabilita ya que por default es la camara frontal
+  const [photo, setPhoto] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -21,77 +20,63 @@ export default function CheckOut() {
     return <Text>No access to camera</Text>;
   }
 
-//   const takePicture = async () => {
-//     if (!Camera) return
-//     const photo = await Camera.takePictureAsync()
-//     console.log(photo)
-//   }
-
-const takePicture = async () => {
-    if (camera) {
-        const options = { quality: 0.7, base64: true };
-        const data = await camera.takePictureAsync(options);
-        const source = data.base64;
-        console.log(source)
-    
-        // if (source) {
-        //   await cameraRef.current.pausePreview();
-        //   setIsPreview(true);
-        // }
-      }
-  }
+  const takePhoto = async () => {
+    if (this.camera) {
+      const options = { quality: 0.7, base64: true };
+      let photo = await this.camera.takePictureAsync(options);
+      setPhoto(photo);
+      console.log(photo.uri);
+    }
+  };
 
   return (
     <View style={{ flex: 1 }}>
       <View style={{ backgroundColor: "white", flex: 2 }}>
         <Text style={{ fontSize: 48, alignSelf: "center" }}>Hola</Text>
-        <Camera style={{ paddingTop: 420 }} type={Camera.Constants.Type.front}>
-        <Text style={{ fontSize: 64, color: "white" }} onPress={() => takePicture()}> Tomar foto </Text>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: "transparent",
-              flexDirection: "row",
-              margin: 20,
+
+        {!photo.uri ? (
+          <>
+            <Camera
+              style={{ paddingTop: 420 }}
+              type={Camera.Constants.Type.front}
+              ref={(ref) => {
+                //Con esta funcion podemos hacer que los metodos de takePictureAsync se habiliten
+                this.camera = ref;
+              }}
+            ></Camera>
+            <View
+              style={{
+                backgroundColor: "gray",
+                height: 80,
+                width: 80,
+                borderRadius: 100,
+                alignSelf: "center",
+                marginTop: 40,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "white",
+                  height: 70,
+                  width: 70,
+                  borderRadius: 100,
+                  alignSelf: "center",
+                  marginTop: 5,
+                }}
+                onPress={takePhoto}
+              ></TouchableOpacity>
+            </View>
+          </>
+        ) : (
+          <Image
+            style={{ paddingTop: 420 }}
+            source={{
+              uri: photo.uri,
             }}
-          >
-            {/* Habilita el boton para cambiar de camara trasera o delantera por requermiento debe ser la frontal */}
-            {/* <TouchableOpacity
-            style={{ flex: 0.1, alignSelf: "flex-end", alignItems: "center" }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.front
-                  ? Camera.Constants.Type.back
-                  : Camera.Constants.Type.front
-              );
-            }}
-          >
-            <Text style={{ fontSize: 18, color: "white" }}> Flip </Text>
-          </TouchableOpacity> */}
-          </View>
-        </Camera>
-        <View
-          style={{
-            backgroundColor: "gray",
-            height: 80,
-            width: 80,
-            borderRadius: 100,
-            alignSelf: "center",
-            marginTop: 40,
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              backgroundColor: "white",
-              height: 70,
-              width: 70,
-              borderRadius: 100,
-              alignSelf: "center",
-              marginTop: 5,
-            }}
-            onPress={() => takePicture()}
-          ></TouchableOpacity>
-        </View>
+          />
+        )}
+
+        {/* <Text>{photo.uri}</Text> */}
       </View>
     </View>
   );
